30da53921e5966db8534cd9323ba542705d8ed73
dagrimb <74441727+dagrimb@users.noreply.github.com>
2021-03-27 16:15:09 -0700

    create MovieCard component

---
+++ src/components/movie-card/movie-card.jsx
@@ -0,0 +1,8 @@
+import React from 'react';
+
+//create MovieCard component
+export class MovieCard extends React.Component {
+  render() {
+    return <div className="movie-card">some title</div>;
+  }
+}
