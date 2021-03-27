8223b5abc38f7c67e4c3addb4f82ba05800b6874
dagrimb <74441727+dagrimb@users.noreply.github.com>
2021-03-26 17:11:44 -0700

    create MainView component

---
+++ src/components/main-view/main-view.jsx
@@ -0,0 +1,15 @@
+//import React into file
+import React from 'react';
+
+//create MainView component as a class component by using React.Component template
+export class MainView extends React.Component {
+  render() {
+    return (
+        <div className="main-view">
+          <div>Inception</div>
+          <div>The Shawshank Redemption</div>
+          <div>Gladiator</div>
+        </div>
+    );
+  }
+}
