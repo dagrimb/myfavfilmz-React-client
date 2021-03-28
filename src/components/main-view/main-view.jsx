78335b879f9a26d728bf70292bf7ffd8ad32eaff
dagrimb <74441727+dagrimb@users.noreply.github.com>
2021-03-27 17:01:03 -0700

    import MovieView and add selectedMovie state to main-view

--- src/components/main-view/main-view.jsx
+++ src/components/main-view/main-view.jsx
@@ -2,6 +2,8 @@
 import React from 'react';
 //import MovieCard into file
 import { MovieCard } from '../movie-card/movie-card';
+//import MovieView into file
+import { MovieView } from '../movie-view/movie-view';
 
 //create MainView component as a class component by using React.Component template
 export class MainView extends React.Component {
@@ -13,12 +15,15 @@ export class MainView extends React.Component {
         { _id: 1, Title: 'Inception', Description: 'desc1...', ImagePath: '...'},
         { _id: 2, Title: 'The Shawshank Redemption', Description: 'desc2...', ImagePath: '...'},
         { _id: 3, Title: 'Gladiator', Description: 'desc3...', ImagePath: '...'}
-      ]
+      ],
+      selectedMovie: null //set default (pre-click event) value to null
     }
   }
 
   render() {
-    const  { movies } = this.state;
+    const  { movies, selectedMovie } = this.state;
+    //if not clicked, access selectedMovie state
+    if (selectedMovie) return <MovieView movie={selectedMovie} />;
     //if no movies, display message stating that the list is empty
     if (movies.length === 0) return <div className="main-view">The list is empty!</div>;
     //else, display list of movie cards
