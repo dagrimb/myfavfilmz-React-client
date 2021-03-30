e55f2f25f8b859278f2a0dff2de89d7d539203ed
dagrimb <74441727+dagrimb@users.noreply.github.com>
2021-03-30 09:56:50 -0700

    Update src.78399e21.js

--- dist/src.78399e21.js
+++ dist/src.78399e21.js
@@ -31827,7 +31827,7 @@ var parent = module.bundle.parent;
 if ((!parent || !parent.isParcelRequire) && typeof WebSocket !== 'undefined') {
   var hostname = "" || location.hostname;
   var protocol = location.protocol === 'https:' ? 'wss' : 'ws';
-  var ws = new WebSocket(protocol + '://' + hostname + ':' + "53760" + '/');
+  var ws = new WebSocket(protocol + '://' + hostname + ':' + "55515" + '/');
 
   ws.onmessage = function (event) {
     checkedAssets = {};
