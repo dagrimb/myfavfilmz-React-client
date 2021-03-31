3a1dbf57a7926935fe1667f40b202e93e09b57cb
dagrimb <74441727+dagrimb@users.noreply.github.com>
2021-03-31 09:08:15 -0700

    updated movie-view object to match structure of JSON object of movies and their props

--- src/components/movie-view/movie-view.jsx
+++ src/components/movie-view/movie-view.jsx
@@ -14,43 +14,49 @@ export class MovieView extends React.Component {
 
     return (
       <div className="movie-view">
-        <div className="movie-poster">
-          <img src={movie.ImagePath} />
+        <div className="movie-description">
+          <span className="label">Description: </span>
+          <span className="value">{movie.Description}</span>
+        </div>
+        <div className="movie-genre">
+          <span className="label">Genre: </span>
+          <span className="value">{movie.Genre}</span>
+        </div>
+        <div className="movie-director">
+          <span className="label">Director: </span>
+          <span className="value">{movie.Director}</span>
+        </div>
+        <div className="movie-actors">
+          <span className="label">Actors: </span>
+          <span className="value">{movie.Actors}</span>
+        </div>
+        <div className="movie-id">
+          <span className="label">ID: </span>
+          <span className="value">{movie._id}</span>
         </div>
         <div className="movie-title">
           <span className="label">Title: </span>
           <span className="value">{movie.Title}</span>
         </div>
-        <div className="movie-description">
-          <span className="label">Description: </span>
-          <span className="value">{movie.Description}</span>
+        <div className="movie-poster">
+          <img src={movie.ImagePath} />
         </div>
 
-        <div className="movie-genre">
-          <span className="label">Genre: </span>
-          <span className="value">{movie.Genre}</span>
+        <div className="movie-featured">
+          <span className="label">Featured: </span>
+          <span className="value">{movie.Featured}</span>
         </div>
 
-        <div className="movie-director">
-          <span className="label">Director: </span>
-          <span className="value">{movie.Director}</span>
+        <div className="movie-score">
+          <span className="label">Rotten Tomatoes score: </span>
+          <span className="value">{movie.Rotten_Tomatoes_score}</span>
         </div>
-
-        <div className="movie-actors">
-          <span className="label">Actors: </span>
-          <span className="value">{movie.Actors}</span>
-        </div>
-
         <div className="movie-year">
           <span className="label">Year Released: </span>
           <span className="value">{movie.Year}</span>
         </div>
-
         <button onClick={() => { onBackClick(null); }}>Back</button>
       </div>
     );
   }
-
-
-
 }
