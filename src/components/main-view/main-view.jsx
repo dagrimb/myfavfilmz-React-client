713e42ab30fdb11f8b031771fe7cadb1ea9a8f64
dagrimb <74441727+dagrimb@users.noreply.github.com>
2021-04-08 08:51:26 -0700

    Update main-view.jsx

--- src/components/main-view/main-view.jsx
+++ src/components/main-view/main-view.jsx
@@ -3,7 +3,7 @@ import React from 'react';
 //import Axios info file
 import axios from 'axios';
 // import LoginView into file
-import { LoginView} from '../login-view/login-view';
+import { LoginView } from '../login-view/login-view';
 //import MovieCard into file
 import { MovieCard } from '../movie-card/movie-card';
 //import MovieView into file
@@ -12,14 +12,14 @@ import { MovieView } from '../movie-view/movie-view';
 //create MainView component as a class component by using React.Component template
 export class MainView extends React.Component {
   //add movies state that will hold list of movies
-  constructor(){
+  constructor() {
 
     super(); //initialize component state
     this.state = {
       movies: null,
       selectedMovie: null, //set default (pre-click event) value to null
       user: null // represents a default state of a user being logged out
-    }
+    };
   }
 
   //Fetch the list of movies from your database with MainView is mounted
@@ -62,7 +62,7 @@ export class MainView extends React.Component {
     //if not clicked, access selectedMovie state (passing a function as a prop called "onMovieClick")
     if (selectedMovie) return <MovieView movie={selectedMovie} onBackClick={newSelectedMovie => { this.setSelectedMovie(newSelectedMovie); }} />;
     //if no movies, display message stating that the list is empty
-    if (movies.length === 0) return <div className="main-view" />;
+    if (!movies || movies.length === 0) return <div className="main-view" />;
     //else, display list of movie cards
     return (
         <div className="main-view">
