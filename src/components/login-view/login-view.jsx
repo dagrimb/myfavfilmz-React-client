5e2c9a6bb2460503c401a79ebfb92e5e6eba7bb3
dagrimb <74441727+dagrimb@users.noreply.github.com>
2021-04-08 08:51:11 -0700

    Update login-view.jsx

--- src/components/login-view/login-view.jsx
+++ src/components/login-view/login-view.jsx
@@ -20,7 +20,7 @@ export function LoginView(props) {
           Password:
           <input type="password" value={password} onChange={e => setPassword(e.target.value)} />
         </label>
-        <button type="button" onClick={handleSubmit}>Submit</button>
+        <button type="submit" onClick={handleSubmit}>Submit</button>
       </form>
     );
   }
