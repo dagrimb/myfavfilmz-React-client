7a8c6789fec74dc4e6b44dbb6388c0faa335bee4
dagrimb <74441727+dagrimb@users.noreply.github.com>
2021-04-11 20:03:16 -0700

    Update login-view.jsx

--- src/components/login-view/login-view.jsx
+++ src/components/login-view/login-view.jsx
@@ -1,26 +1,38 @@
+//Import React and useState hook
 import React, { useState } from 'react';
+import PropTypes from 'prop-types';
 
+//call useState method and set to empty string the represents values prior to login
 export function LoginView(props) {
-  const [ username, setUsername ] = useState(''); // '' is initial value of login variable
+  const [ username, setUsername ] = useState('');
   const [ password, setPassword ] = useState('');
 
-  const handleSubmit = (e) => {
+  //Send request to server for auth
+  const handleSubmit = () => {
     e.preventDefault();
     console.log(username, password);
-    // Send a request to the server for authentication then call props on LoggedIn(username)
     props.onLoggedIn(username);
-  }
-    return (
-      <form>
-        <label>
-          Username:
-          <input type="text" value={username} onChange={e => setUsername(e.target.value)} />
-        </label>
-        <label>
-          Password:
-          <input type="password" value={password} onChange={e => setPassword(e.target.value)} />
-        </label>
-        <button type="submit" onClick={handleSubmit}>Submit</button>
-      </form>
-    );
-  }
+  };
+
+  return (
+    <form>
+      <label>
+        Username:
+        <input type="text" value={username} onChange={e => setUsername(e.target.value)} />
+      </label>
+      <label>
+        Password:
+        <input type="password" value={password} onChange={e => setPassword(e.target.value)} />
+      </label>
+      <button type="submit" onClick={handleSubmit}>Submit</button>
+    </form>
+  );
+}
+
+LoginView.propTypes = {
+  username: PropTypes.shape({
+    Username: PropTypes.string.isRequired,
+    Password: PropTypes.string.isRequired,
+  }).isRequired,
+  onClick: PropTypes.func.isRequired
+};
