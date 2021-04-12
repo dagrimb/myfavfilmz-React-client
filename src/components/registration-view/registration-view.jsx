4e7c68ec1411df19f7b0d7e5b73895aeb80d0743
dagrimb <74441727+dagrimb@users.noreply.github.com>
2021-04-11 19:16:29 -0700

    create RegistrationView functional component, implementing useState and PropTypes

---
+++ src/components/registration-view/registration-view.jsx
@@ -0,0 +1,51 @@
+//Import React and useState hook
+import React, { useState } from 'react';
+import PropTypes from 'prop-types';
+
+//call useState method and set to empty string the represents values prior to login
+export function RegistrationView(props) {
+  const [ username, setUsername ] = useState('');
+  const [ password, setPassword ] = useState('');
+  const [ email, setEmail ] = useState('');
+  const [ birthday, setBirthday ] = useState(new Date());
+
+  //Send request to server for auth
+  const handleSubmit = () => {
+    e.preventDefault();
+    console.log(username, password);
+    props.onRegistered(username);
+  };
+
+  return (
+    <form>
+      <label>
+        Username:
+        <input type="text" value={username} onChange={e => setUsername(e.target.value)} />
+      </label>
+      <label>
+        Password:
+        <input type="password" value={password} onChange={e => setPassword(e.target.value)} />
+      </label>
+      <label>
+        Email:
+        <input type="email" value={email} onChange={e => setEmail(e.target.value)} />
+      </label>
+      <label>
+        Birthday:
+        <input type="date" value={birthday} onChange={e => setBirthday(e.target.value)} />
+      </label>
+      <button type="submit" onClick={handleSubmit}>Submit</button>
+    </form>
+  );
+}
+
+RegistrationView.propTypes = {
+  username: PropTypes.shape({
+    FavoriteMovies: PropTypes.array.isRequired,    
+    Username: PropTypes.string.isRequired,
+    Password: PropTypes.string.isRequired,
+    Email: PropTypes.string.isRequired,
+    Birthday: PropTypes.instanceOf(Date)
+  }),
+  onClick: PropTypes.func
+};
