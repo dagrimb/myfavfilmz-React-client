b7d957d0b04712d229f61bf6cc6a36713b59575b
dagrimb <74441727+dagrimb@users.noreply.github.com>
2021-04-11 20:03:11 -0700

    Update movie-card.jsx

--- src/components/movie-card/movie-card.jsx
+++ src/components/movie-card/movie-card.jsx
@@ -4,17 +4,38 @@ import PropTypes from 'prop-types';
 //create MovieCard component
 export class MovieCard extends React.Component {
   render() {
-    const { movie, onClick } = this.props; 
+    const { movie, onClick } = this.props;
 
     return (
-    <div onClick={() => onClick(movie)} className="movie-card">{movie.Title}</div>
+       <div onClick={() => onClick(movie)} className="movie-card">{movie.Title}</div>
     );
   }
 }
 
-MovieCard.PropTypes = {
-  movie: PropTypes.shape({ // movie object
-    Title: PropTypes.string
+MovieCard.propTypes = {
+  movie: PropTypes.shape({
+    Description: PropTypes.shape({
+      Synopsis: PropTypes.string.isRequired,
+      Source: PropTypes.string.isRequired
+    }),
+    Genre: PropTypes.shape({
+      Name: PropTypes.string.isRequired,
+      Description: PropTypes.string.isRequired,
+      Source: PropTypes.string.isRequired
+    }),                   
+    Director: PropTypes.shape({
+      Name: PropTypes.string.isRequired,
+      Bio: PropTypes.string.isRequired,
+      Birth: PropTypes.string.isRequired,
+      Source: PropTypes.string.isRequired
+    }),
+    Actors: PropTypes.array.isRequired,
+    _id: PropTypes.string.isRequired,    
+    Title: PropTypes.string.isRequired,
+    ImagePath: PropTypes.string.isRequired,
+    Featured: PropTypes.bool.isRequired,
+    Rotten_Tomatoes_score: PropTypes.string.isRequired,
+    Year: PropTypes.string.isRequired
   }).isRequired,
   onClick: PropTypes.func.isRequired
 };
