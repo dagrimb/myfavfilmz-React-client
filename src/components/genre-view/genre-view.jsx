160648858417470ea991227800709f236b0cea49
dagrimb <74441727+dagrimb@users.noreply.github.com>
2021-06-10 18:31:12 -0700

    remove additional navbar and style body of component

--- src/components/genre-view/genre-view.jsx
+++ src/components/genre-view/genre-view.jsx
@@ -32,25 +32,12 @@ export class GenreView extends React.Component {
     return (
       <div className="genre-view">
         <Row className="main-view justify-content-md-center ml-0 h-100">
-        <div className="w-100">
-          <Navbar bg="primary" variant="dark" style={{paddingLeft: 0, paddingRight: 0 }}>
-            <Navbar.Brand className="ml-2" href="#home">myfavfilmz</Navbar.Brand>
-            <Nav className="mr-auto">
-              <Nav.Link href="#account">Account</Nav.Link>
-              <Nav.Link href="#movies">Movies</Nav.Link>
-              <Nav.Link href="#about">About</Nav.Link>
-            </Nav>
-            <Form inline>
-              <FormControl type="text" placeholder="Search" className="mr-3" />
-              <Button variant="outline-light" className="mr-5">Search</Button>
-
-            </Form>
-          </Navbar> 
+          <div className="w-100">
           </div>
         </Row>
         <Col className="pt-5 bg-dark" style={{ paddingBottom: '80rem', height: '100%', width: '100%', color: 'white', background: '#292b2c'}}>
-          <div class="mx-auto" style={{ height: '100%', maxWidth: '50%' }} >
-            <div className="movie-genre" style={{ color: 'white' }}>
+          <div class="mx-auto" style={{ height: '100%', maxWidth: '100%' }} >
+            <div className="movie-genre" style={{ color: 'white', textAlign: 'center' }}>
             <div className="movie-poster mb-4 justify-content-center text-center mx-auto" fluid>
               <img src={genre.ImagePath} height="5%" width="20%"/>
             </div>
