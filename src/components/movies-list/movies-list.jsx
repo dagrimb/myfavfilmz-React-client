0545b9df021445ca028ca6ef375cf36bee9d5a3c
dagrimb <74441727+dagrimb@users.noreply.github.com>
2021-07-14 10:47:29 -0700

    pass addFavorite function to MovieCard

--- src/components/movies-list/movies-list.jsx
+++ src/components/movies-list/movies-list.jsx
@@ -4,16 +4,13 @@ import { connect } from 'react-redux';
 
 import { MovieCard } from '../movie-card/movie-card';
 
-import VisibilityFilterInput from '../visibility-filter-input/visibility-filter-input';
-import { MovieCard } from '../movie-card/movie-card';
-
 const mapStateToProps = state => {
   const { visibilityFilter } = state;
   return { visibilityFilter };
 };
 
 function MoviesList(props) {
-  const { movies, visibilityFilter } = props;
+  const { movies, visibilityFilter, addFavorite } = props;
   let filteredMovies = movies;
 
   if (visibilityFilter !== '') {
@@ -25,7 +22,7 @@ function MoviesList(props) {
   return <>
     {filteredMovies.map(m => (
       <Col variant="h-50" className="bg-dark" xs={12} sm={8} md={4} lg={4} xl={3}  key={m._id}>
-        <MovieCard movie={m} />
+        <MovieCard movie={m} addFavorite={addFavorite} />
       </Col>
     ))}
   </>;
