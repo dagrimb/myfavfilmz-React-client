5cb7b9e51523452439a961a3e985757f092f3365
dagrimb <74441727+dagrimb@users.noreply.github.com>
2021-07-14 10:49:15 -0700

    create reducers to handle user and favorite movie actions

--- src/reducers/reducers.js
+++ src/reducers/reducers.js
@@ -1,6 +1,9 @@
 import { combineReducers } from 'redux';
 
-import { SET_FILTER, SET_MOVIES, SET_USER } from '../actions/actions';
+import { SET_FILTER, SET_MOVIES, SET_USER, SET_FAVORITES, UPDATE_INFO, ADD_FAVORITE, 
+REMOVE_FAVORITE } from '../actions/actions';
+
+
 
 function visibilityFilter(state = '', action) { // signature, or identity card, of reducer
   switch (action.type) {
@@ -20,20 +23,36 @@ function movies(state = [], action) { // signature, or identity card, of reducer
   }
 }
 
-function user(state = '', action) {
+function user(state = [], action) {
   switch (action.type) {
-    case SET_USER:
+    case SET_USER: // via login
+      return action.value;
+    case UPDATE_INFO: // via profile edit view form
       return action.value;
     default:
       return state;
   }
 }
 
+function favorites(state = [], action) {
+  switch (action.type) {
+    case SET_FAVORITES: // via logging
+      return action.value;
+    case ADD_FAVORITE: // via the two buttons to add movies
+      return state.concat((id) => id === action.id);
+    case REMOVE_FAVORITE: // via the remove button feature
+      return state.filter((id) => id !== action.id);
+    default:
+        return state;
+  }
+}
+
 // groups all the reducers together and only passes them the state they're concerned with
-const moviesApp = combineReducers({
+const moviesApp = combineReducers({ 
     visibilityFilter,
     movies,
-    user
+    user,
+    favorites
   });
 
 
