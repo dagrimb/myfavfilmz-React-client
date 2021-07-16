90cb826be7265eac99866e6e9c3b45e38b8c48af
dagrimb <74441727+dagrimb@users.noreply.github.com>
2021-07-15 20:44:24 -0700

    make stying changes to improve responsiveness to screen size change

--- src/components/director-view/director-view.jsx
+++ src/components/director-view/director-view.jsx
@@ -1,13 +1,7 @@
 import React from 'react'; 
-import Image from 'react-bootstrap/Image';
-import Row from 'react-bootstrap/Row';
 import Col from 'react-bootstrap/Col';
 import PropTypes from 'prop-types';
-import { MovieView } from '../movie-view/movie-view';
-import { Navbar,Nav,NavDropdown,Form,FormControl} from 'react-bootstrap';
-//import { Button } from '../button/button';
 import Button from 'react-bootstrap/Button';
-import { Link } from 'react-router-dom';
 
 //create DirectorView component
 export class DirectorView extends React.Component {
@@ -28,23 +22,20 @@ export class DirectorView extends React.Component {
   render() {
     const { onBackClick, director } = this.props;
 
-
     return (
       <div className="director-view">
-        <Row className="main-view justify-content-md-center ml-0 h-100">
-        <div className="w-100">
-          </div>
-        </Row>
-        <Col className="pt-5 bg-dark" style={{ paddingBottom: '105rem', height: '100%', width: '100%', color: 'white', background: '#292b2c'}}>
-          <div class="mx-auto" style={{ height: '100%', maxWidth: '50%' }} >
-            <div className="movie-director" style={{ color: 'white' }}>
+        <Col xs={10} sm={7} md={6} lg={4} xl={3} className="pt-5 bg-dark mx-auto" style={{ paddingBottom: '20rem', height: '50%', width: '100%', color: 'white', background: '#292b2c'}}>
+          <div class="mx-auto" style={{ width: '100%' }} >
+            <div className="movie-director" style={{ color: 'white', width: '100%' }}>
             <div className="movie-poster mb-4 justify-content-center text-center mx-auto" fluid>
-              <img src={director.ImagePath} height="5%" width="20%"/>
+              <img src={director.ImagePath} height="100%" width="80%"/>
             </div>
+            <div>
               <span className="value">{director.Name}</span>
               <span className="value"> ({director.Birth}-{director.Death}). </span>
               <span className="value">{director.Bio} </span>
               <a className="label" href={director.Source}>(source)</a>  
+            </div>
             </div>
             <div style={{ textAlign: 'center' }}>
               <Button className="mt-5"  variant="primary" onClick={() => { onBackClick(null); }}>Back</Button>
