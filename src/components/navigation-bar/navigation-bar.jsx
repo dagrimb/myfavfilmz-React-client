764cc0db73e0c8f49e04a5eaba241052a47d9fa7
dagrimb <74441727+dagrimb@users.noreply.github.com>
2021-07-15 20:47:42 -0700

    make stying changes to improve responsiveness to screen size change and remove unused code

--- src/components/navigation-bar/navigation-bar.jsx
+++ src/components/navigation-bar/navigation-bar.jsx
@@ -1,6 +1,6 @@
-import React, {Fragment} from 'react'; 
-import { FaveMovies } from '../fave-movies/fave-movies';
+import React from 'react'; 
 
+import { Link } from 'react-router-dom';
 import { connect } from 'react-redux';
 
 import VisibilityFilterInput from '../visibility-filter-input/visibility-filter-input';
@@ -10,53 +10,49 @@ const mapStateToProps = state => {
   return { visibilityFilter };
 };
 
-
-import Image from 'react-bootstrap/Image';
-import axios from 'axios';
-
 import Row from 'react-bootstrap/Row';
-import Col from 'react-bootstrap/Col';
-import PropTypes from 'prop-types';
-import { Navbar,Nav,NavDropdown,Form,FormControl} from 'react-bootstrap';
-import { ProfileView } from '../profile-view/profile-view';
-
-import { BrowserRouter as Router, Route, Link} from "react-router-dom";
-
-//import { Button } from '../button/button';
+import { Navbar,Nav,Form} from 'react-bootstrap';
+import { BrowserRouter as Router, Link} from "react-router-dom";
 import Button from 'react-bootstrap/Button';
-import Card from 'react-bootstrap/Card';
-import CardDeck from 'react-bootstrap/CardDeck';
+
 
 //create MovieView component
 function NavigationBar(props) {
-  const { movies, user, onLoggedOut, visibilityFilter } = props;
-
-
+  const { user, onLoggedOut, visibilityFilter } = props;
 
     return (
       <Router>
-      <Row className="main-view justify-content-md-center ml-0 w-100 bg-dark">
-        <div className="w-100">
-          <Navbar bg="primary" variant="dark">
-            <Navbar.Brand className="ml-2" href="#home">myfavfilmz</Navbar.Brand>
-            <Nav className="mr-auto">
-              <Nav.Link href="#account">Account</Nav.Link>
-              <Nav.Link href="#movies">Movies</Nav.Link>
-              <Nav.Link href="aboutus">About</Nav.Link>
-            </Nav>
-            <Form inline>
-            <VisibilityFilterInput visibilityFilter={visibilityFilter} />
-              <Link to={`/users/${user.Username}`}>
-                <Button variant="link-white" onClick="window.location.reload()">{user.Username}</Button>
-              </Link>
-              <button onClick={onLoggedOut}>Logout</button>
-            </Form>
-          </Navbar>
-        </div>
+        <Row className="main-view justify-content-md-center mx-auto w-100 bg-dark" >
+          <div className="w-100">
+            <Navbar collapseOnSelect bg="primary" variant="dark" expand="xl" className="w-100">
+              <Navbar.Brand as={Link} to="/">myfavfilmz</Navbar.Brand>
+              <Navbar.Toggle aria-controls="responsive-navbar-nav" />
+              <Navbar.Collapse id="responsive-navbar-nav" className="w-90">
+                <Nav style={{ marginLeft: '1rem', width: '30%' }}>
+                  <Nav.Link as={Link} to="/account" variant="transparent" className="ml-1">
+                    <Button>Account</Button>
+                  </Nav.Link>
+                  <Nav.Link as={Link} to="/about" variant="transparent">
+                    <Button>About</Button>
+                  </Nav.Link>
+                </Nav>
+                <Form style={{ width: '100%', textAlign: 'center' }} className="w-sm-100">
+                  <div className="my-4">
+                    <VisibilityFilterInput className="mt-4" visibilityFilter={visibilityFilter} />
+                  </div>
+                  <div className="my-4 mx-auto">           
+                  <Link to={`/users/${user.Username}`}>
+                    <Button className="mx-4 " variant="link-white" onClick="window.location.reload()">{user.Username}'s profile</Button>
+                  </Link>
+                  <button style={{ marginRight: '0' }} onClick={onLoggedOut}>Logout</button>
+                  </div>  
+                </Form>
+              </Navbar.Collapse>
+            </Navbar>
+          </div>
         </Row>
-        </Router>            
-          );
-        }
+      </Router>            
+      );
+    }
       
-
-      export default connect(mapStateToProps)(NavigationBar);
+  export default connect(mapStateToProps)(NavigationBar);
