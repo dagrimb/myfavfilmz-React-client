7d9866d7cdc11266e62c41af715af3dbb05a4f3c
dagrimb <74441727+dagrimb@users.noreply.github.com>
2021-07-15 20:47:33 -0700

    add conditional to render based on whether user has fave movies

--- src/components/movies-list/movies-list.jsx
+++ src/components/movies-list/movies-list.jsx
@@ -16,16 +16,22 @@ function MoviesList(props) {
   if (visibilityFilter !== '') {
     filteredMovies = movies.filter(m => m.Title.toLowerCase().includes(visibilityFilter.toLowerCase()));
   }
-
-  if (!movies) return <div className="main-view"/>;
-
-  return <>
-    {filteredMovies.map(m => (
-      <Col variant="h-50" className="bg-dark" xs={12} sm={8} md={4} lg={4} xl={3}  key={m._id}>
-        <MovieCard movie={m} addFavorite={addFavorite} />
-      </Col>
-    ))}
-  </>;
+  
+  return (
+    <>
+      {
+        filteredMovies.length > 0 ? 
+          filteredMovies.map(m => 
+            <Col variant="h-50" className="bg-dark justify-md-content-center" xs={12} sm={10} md={4} lg={4} xl={3} xxl={2} key={m._id}>
+              <MovieCard movie={m} addFavorite={addFavorite} />
+            </Col>
+          )
+        : 
+          <div className="bg-dark justify-md-content-center m-5" style={{ color: 'white', height: '35rem'}}>No movies found. Please check the 
+          name of the film you are looking for and try again.</div>
+      }
+    </>
+  )
 }
 
   export default connect(mapStateToProps)(MoviesList);
