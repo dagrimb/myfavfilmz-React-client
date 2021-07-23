94d82a09207f0f40a59a7acfed6e95fb283fed07
dagrimb <74441727+dagrimb@users.noreply.github.com>
2021-07-23 16:47:44 -0700

    Revert "refactor FavoriteMovie prop passed to ProfileView function that gets the movies in this collection"
    This reverts commit 0586d863ab16b0b4036d3b30ed4607400014ee49.

--- src/components/main-view/main-view.jsx
+++ src/components/main-view/main-view.jsx
@@ -299,8 +299,7 @@ class MainView extends React.Component {
               return (
                 <>
                   <NavigationBar FavoriteMovie={FavoriteMovies} user={user} onLoggedIn={user => this.onLoggedIn(user)} onLoggedOut={user => this.onLoggedOut(user)} />
-                  <ProfileView FavoriteMovie={user.FavoriteMovies.map(movieID => movies.find(m => m._id === movieID))} 
-                  user={user} removeFavoriteFilm={movie => this.removeFavoriteFilm(movie)} onBackClick={() => history.goBack()}/>
+                  <ProfileView FavoriteMovie={FavoriteMovies} user={user} removeFavoriteFilm={movie => this.removeFavoriteFilm(movie)} onBackClick={() => history.goBack()}/>
                 </>
               )
             }} />
