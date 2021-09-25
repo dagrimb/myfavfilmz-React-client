611ff72a3a498eee7803722bcc3c913a99afb3bc
dagrimb <74441727+dagrimb@users.noreply.github.com>
2021-09-25 14:07:47 -0700

    Update index.jsx

--- src/index.jsx
+++ src/index.jsx
@@ -1,20 +1,22 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import MainView from './components/main-view/main-view';
 import Container from 'react-bootstrap/Container';
-import { createStore } from 'redux';
+//import { createStore } from 'redux';
 import { Provider } from 'react-redux';
+import { configureStore } from './store/configureStore';
+
 import moviesApp from './reducers/reducers';
 
 
 
 import { devToolsEnhancer } from 'redux-devtools-extension';
 
-import MainView from './components/main-view/main-view';
 // Import statement that indicates the need to bundle `./index.scss`
 import './index.scss';
 
-const store = createStore(moviesApp, devToolsEnhancer());
-
+//const store = createStore(moviesApp, devToolsEnhancer());
+const store = configureStore();
 
 // Main component (will eventually use all the others)
 class myfavfilmzApplication extends React.Component {
