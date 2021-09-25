6b7c0404a8364e8637a7f9662e48ed10bcb5dacf
dagrimb <74441727+dagrimb@users.noreply.github.com>
2021-09-25 14:07:39 -0700

    Update navigation-bar.jsx

--- src/components/navigation-bar/navigation-bar.jsx
+++ src/components/navigation-bar/navigation-bar.jsx
@@ -2,13 +2,10 @@ import React from 'react';
 
 import { Link } from 'react-router-dom';
 import { connect } from 'react-redux';
-
+import * as Actions from '../../actions/actions';
 import VisibilityFilterInput from '../visibility-filter-input/visibility-filter-input';
 
-const mapStateToProps = state => {
-  const { visibilityFilter } = state;
-  return { visibilityFilter };
-};
+
 
 import Row from 'react-bootstrap/Row';
 import { Navbar,Nav,Form} from 'react-bootstrap';
@@ -17,8 +14,16 @@ import Button from 'react-bootstrap/Button';
 
 
 //create MovieView component
-function NavigationBar(props) {
-  const { user, onLoggedOut, visibilityFilter } = props;
+class NavigationBar extends React.Component {
+
+  onLoggedOut() {
+    localStorage.removeItem('token');
+    localStorage.removeItem('userID');
+    this.props.logoutUser();
+  }
+
+  render() {
+    const { user, visibilityFilter } = this.props;
 
     return (
       <Router>
@@ -44,7 +49,7 @@ function NavigationBar(props) {
                   <Link to={`/users/${user.Username}`}>
                     <Button className="mx-4 " variant="link-white" onClick="window.location.reload()">{user.Username}'s profile</Button>
                   </Link>
-                  <button style={{ marginRight: '0' }} onClick={onLoggedOut}>Logout</button>
+                  <button style={{ marginRight: '0' }} onClick={() => this.onLoggedOut()}>Logout</button>
                   </div>  
                 </Form>
               </Navbar.Collapse>
@@ -54,5 +59,14 @@ function NavigationBar(props) {
       </Router>            
       );
     }
-      
-  export default connect(mapStateToProps)(NavigationBar);
+  }
+  
+  function mapStateToProps(state) {
+      const { visibilityFilter } = state;
+      return { 
+        visibilityFilter, 
+        loggedIn: state.loggedIn 
+      };
+    };
+
+  export default connect(mapStateToProps, Actions)(NavigationBar);
