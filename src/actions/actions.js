4eb6ddea2aaaffee7313f25f17dd81b91c2ed9b1
dagrimb <74441727+dagrimb@users.noreply.github.com>
2021-11-04 16:44:40 -0700

    Update actions.js

--- src/actions/actions.js
+++ src/actions/actions.js
@@ -1,4 +1,3 @@
-
 export const SET_MOVIES = 'SET_MOVIES'; // initialize the movies list with movies
 export const SET_FILTER = 'SET_FILTER'; // sets a filter to filter movies list
 export const SET_USER = 'SET_USER';
@@ -8,8 +7,6 @@ export const UPDATE_INFO = 'UPDATE_INFO';
 export const SET_FAVORITES = 'SET_FAVORITE';
 export const ADD_FAVORITE = 'ADD_FAVORITE';
 export const REMOVE_FAVORITE = 'REMOVE_FAVORITE';
-export const LOGIN_USER = 'LOGIN_USER';
-export const LOGOUT_USER = 'LOGOUT_USER';
 
 
 export function setMovies(value) {
@@ -55,14 +52,12 @@ export function setFavorites(value) {
   }
 }
 
-/*export function addFavorite(id) {
+export function addFavorite(id) {
   return {
     type: ADD_FAVORITE,
     id
   }
-}*/
-
-
+}
 
 export function removeFavorite(id) {
   return {
@@ -70,18 +65,3 @@ export function removeFavorite(id) {
     id
   }
 }
-
-export function loginUser() {
-  return {
-    type: LOGIN_USER
-  }
-}
-
-export function logoutUser() {
-  return {
-    type: LOGOUT_USER
-  }
-}
-
-
-
